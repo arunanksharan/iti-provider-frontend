670e4c2694de0a7fb27db93ffeeaa48021e36d35
arunanksharan <arunank.sharan@gmail.com>
2025-03-26 22:59:42 +0530

    [init]: dd assets and babel config for compilation

---
+++ babel.config.js
@@ -0,0 +1,25 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@': './src',
+            '@components': './src/components',
+            '@services': './src/services',
+            '@store': './src/store',
+            '@theme': './src/theme',
+            '@hooks': './src/hooks',
+            '@utils': './src/utils',
+            '@types': './src/types'
+          },
+          extensions: ['.js', '.jsx', '.ts', '.tsx']
+        }
+      ]
+    ]
+  };
+};
